a935a92a7eb3cd09383beaaba231fa3f7e5e85ed
Brett Allred <brettallred@gmail.com>
2018-04-17 09:14:40 -0600

    adding latest changes from current computer before transfer to new computer:

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,3 +9,5 @@ alias goclean='rm -R vendor && rm -R Godeps'
 alias rastage= "ssh -L 15675:localhost:15672 rabbit01-core-stage.awse.nuviapp.com"
 alias raprod= "ssh -L 15674:localhost:15672 rabbit01-core-prod.awse.nuviapp.com"
 
+alias bigfoot_prod='ssh -L 9205:localhost:9200 esc02-bigfoot-prod.awse.nuviapp.com'
+alias bigfoot_stage='ssh -L 9206:localhost:9200 es01-bigfoot-stage.awse.nuviapp.com'
